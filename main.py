dd61fd45a24b7797fca27fe04394422df0b0defb
arbiprog <arbiprog@gmail.com>
2023-07-29 13:12:22 +0100

    signals and slots

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton
 import sys
-from PySide6.QtWidgets import QApplication 
-from Button_holder import new
-
-
 app=QApplication(sys.argv)
 
-window=new()
 
+
+
+window = QMainWindow()
+window.setWindowTitle("إنتاجية")
+window.resize(500,500)
+
+button=QPushButton()
+button.setText("clicked me")
+button.setFixedSize(120,50)
+
+window.setCentralWidget(button)
+
+def button_clicked():
+    print("the button have clicked")
+button.clicked.connect(button_clicked())
 
 window.show()
 app.exec()
