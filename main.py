47fed3c7dfd0e7b781d1f4e6a8272f1e17672837
arbiprog <arbiprog@gmail.com>
2023-07-29 15:56:12 +0100

    slider

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton
-import sys
-app=QApplication(sys.argv)
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QSlider, QWidget
 
+def respond_to_slider(data):
+    print("slider moved to:", data)
 
+app = QApplication()
 
+# Create a custom widget
+widget = QWidget()
+widget.setFixedSize(500, 500)  # Set the fixed size to 500x500 pixels
 
-window = QMainWindow()
-window.setWindowTitle("إنتاجية")
-window.resize(500,500)
+slider = QSlider(Qt.Horizontal, widget)  # Add the slider to the custom widget
+slider.setMinimum(0)
+slider.setMaximum(100)
+slider.setValue(50)
 
-button=QPushButton()
-button.setText("clicked me")
-button.setFixedSize(120,50)
+slider.valueChanged.connect(respond_to_slider)
 
-window.setCentralWidget(button)
-
-def button_clicked():
-    print("the button have clicked")
-button.clicked.connect(button_clicked())
-
-window.show()
+widget.show()
 app.exec()
